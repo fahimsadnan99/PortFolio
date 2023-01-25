7662ba47179dcf295063e4eb3a20ec01a9b5ae9e
fahimsadnan99 <fahimgithub@gmail.com>
2023-01-25 17:54:42 +0600

    intro add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './layout/Navbar'
 import CopyRight from "./components/Home/CopyRight/CopyRight"
+import Intro  from "./assets/intro.gif"
 const App = () => {
+  let [intros,setIntros] = useState(true)
+console.log(intros);
+  useEffect(()=>{
+     let timeOut = setTimeout(()=>{
+      setIntros(false)
+     },2000)
+     return ()=> clearTimeout(timeOut)
+  },[])
+  
+
   return (
     <>
-    <Navbar></Navbar>
-    <Outlet />
-    <CopyRight></CopyRight>
+    {intros ? ( <div className='h-screen w-full'>
+        <img src={Intro} className='h-screen w-full' />
+      </div>) : (<>
+         <Navbar></Navbar>
+         <Outlet />
+         <CopyRight></CopyRight>
+  </>
+      )
+    }
+   
     </>
   )
 }
