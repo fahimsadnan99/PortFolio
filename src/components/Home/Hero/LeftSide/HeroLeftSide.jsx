730ff6fdf5f02898e4ff84c13413790a278e81ff
unknown <fahimgithub@gmail.com>
2023-04-04 06:23:20 +0100

    cv add

--- src/components/Home/Hero/LeftSide/HeroLeftSide.jsx
+++ src/components/Home/Hero/LeftSide/HeroLeftSide.jsx
@@ -7,7 +7,7 @@ const HeroLeftSide = () => {
         <div>
         <p className='sm:text-5xl md:text-6xl font-mukta py-2 xs:text-4xl'>I am Fahim Sadnan</p>
         <p className='sm:text-2xl md:text-3xl py-2 font-Satisfy xs:text-2xl'>Full Stack Web Developer</p>
-        <a href={Resume} download="Fahim.Resume" > 
+        <a href="https://docs.google.com/document/d/1T5HHauR7XIVQ0-l49zuTjsA-ugkDEb0I/edit?usp=share_link&ouid=110250861038825363968&rtpof=true&sd=true" download="Fahim.Resume" > 
         <button className='text-md border-1 ring-1 p-2 rounded-sm mt-3 shadow-md shadow-lime-50/30 font-bold'>Download Cv</button></a>
         </div>
 
